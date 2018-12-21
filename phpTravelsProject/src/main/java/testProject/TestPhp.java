cb8ede599bb7fd3ade2cbc81ba15a294dcbd4345
Ashish A. Bhalerao <Ashish A. Bhalerao@DELL>
2018-12-21 05:52:22 +0530

    project added.

---
+++ phpTravelsProject/src/main/java/testProject/TestPhp.java
@@ -0,0 +1,19 @@
+package testProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestPhp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+		System.setProperty("webdrive.chrome.driver", "C:\\Users\\Ashish\\Desktop\\Drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		}finally {
+			
+			
+		}
+	}
+
+}
